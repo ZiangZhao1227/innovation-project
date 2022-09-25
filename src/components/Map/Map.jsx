fc77a1139c5d2c40cae5b3d101f4cb741c64477f
ZIANG ZHAO <zhaoziang1227@gmail.com>
2022-09-25 18:08:10 +0300

    chore: add google map api

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,22 @@
+import GoogleMapReact from "google-map-react";
+import { Box } from "@mui/material";
+
+// ----------------------------------------------------------------------
+
 const Map = () => {
+  const coordinates = { lat: 0, lng: 0 };
+
   return (
-    <div>Map</div>
+    <Box sx={{height:'85vh', width: '100%'}}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+      >
+      </GoogleMapReact>
+    </Box>
   )
 }
 
