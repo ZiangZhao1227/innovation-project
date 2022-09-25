5ac4a4ec991f3012cf454b4a44635b8a3119d76c
ZIANG ZHAO <zhaoziang1227@gmail.com>
2022-09-25 18:11:42 +0300

    chore: set initial coordinates at Helsinki

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,7 +4,7 @@ import { Box } from "@mui/material";
 // ----------------------------------------------------------------------
 
 const Map = () => {
-  const coordinates = { lat: 0, lng: 0 };
+  const coordinates = { lat: 60.192059, lng: 24.945831 };
 
   return (
     <Box sx={{height:'85vh', width: '100%'}}>
@@ -12,7 +12,7 @@ const Map = () => {
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
         defaultCenter={coordinates}
         center={coordinates}
-        defaultZoom={14}
+        defaultZoom={12}
         margin={[50, 50, 50, 50]}
       >
       </GoogleMapReact>
