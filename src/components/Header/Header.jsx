44c218e95c11b4ba66933263f6668e8ba20c5be3
ZIANG ZHAO <zhaoziang1227@gmail.com>
2022-09-25 17:20:43 +0300

    feat: add app top bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,37 @@
+// mui
+import { Typography, Toolbar, Box, AppBar } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+// styles
+import { Search, SearchIconWrapper, StyledInputBase } from './styles.js';
+
+// ----------------------------------------------------------------------
+
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Tourstly
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { CssBaseline, Grid } from '@material-ui/core';
 
-//components
+// components
 import Header from '../components/Header/Header';
 import List from '../components/List/List';
 import Map from '../components/Map/Map';
